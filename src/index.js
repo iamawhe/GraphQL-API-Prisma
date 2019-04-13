3e69808e76744323a07a530cdc960f296a3fec7b
Rookieblue <7204868+iamawhe@users.noreply.github.com>
2019-04-13 08:58:25 +0200

    add subscription resolver and require module in index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
 
 //2 - Resolver fnc (implementation)
 const resolvers = {
 	Query,
 	Mutation,
 	User,
-	Link
+	Link,
+	Subscription
 };
 
 //3
