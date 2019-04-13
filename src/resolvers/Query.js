2bc270f765e6742cee0840577dbbdebbc657920e
Rookieblue <7204868+iamawhe@users.noreply.github.com>
2019-04-13 16:42:56 +0200

    add feed filtering

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,16 @@
-const feed = ( root, args, context, info ) => {
-    return context.prisma.links();
-}
+const feed = async (root, args, context, info) => {
+	const where = args.filter
+		? {
+				OR: [
+					{ description_contains: args.filter },
+					{ url_contains: args.filter }
+				]
+		  }
+		: {};
+	const links = await context.prisma.links({ where });
+	return links;
+};
 
 module.exports = {
-    feed
-}
+	feed
+};
